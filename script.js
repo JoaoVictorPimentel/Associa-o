7e95a45c0461276002ce8062374de8bbcb15f1a6
João Victor Pimentel <joaovitorcavalcante009@gmail.com>
2024-12-13 21:33:25 -0300

    Fix: Correções no header

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const menu = document.getElementById('mobileMenu');
     menu.classList.toggle('active');
 }
+
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
